088771040f8425c505f3ca341d71ff7ae20b7bf6
Santiago Otálvaro <sotalvaroo1999@gmail.com>
2024-06-27 23:00:47 -0500

    Common module created

---
+++ ChopeBastillaApplicationService.CommonDomain/CommonException/DomainException.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ChopeBastillaApplicationService.CommonDomain.CommonException
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string? message) : base(message)
+        {
+        }
+
+        public DomainException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
